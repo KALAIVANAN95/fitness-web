e8e7509bfbb382118b080a6dfc0698b16efca42a
KALAIVANAN <101535641+KALAIVANAN95@users.noreply.github.com>
2026-09-11 17:18:50 +0530

    Update alt text for hero images

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -81,11 +81,11 @@ const Hero = () => {
 
         {/* hero images */}
 
-        <img src={hero_image} alt="Fitness training"
+        <img src={hero_image}  alt="Person doing a fitness workout"
  className="hero-image" />
         <img
           src={hero_image_back}
-           alt="fitness-background"
+           alt=""
           className="hero-image-back"
         />
 
